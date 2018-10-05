6205334d648cd527214777555d01c3b49e875d3d
wcgoodman <wcgoodman@gmail.com>
2018-10-05 11:43:51 -0500

    Added new copy of test shell script

---
+++ test2.sh
@@ -0,0 +1,9 @@
+ping bullhorn.com
+
+mkdir folder
+
+curl -O https://www.stlcc.edu/images/services/multi-use/path-to-student-success-student-version-2x.jpg
+
+mv path-to-student-success-student-version-2x.jpg folder
+
+ll folder
