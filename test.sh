fb181d39f37d307a1e6f79c29cce044448ae9da5
Trent Storch <tstorch@bullhorn.com>
2018-10-05 10:36:33 -0500

    adding ll command to see folder dir

--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@ mkdir folder
 curl -O https://www.stlcc.edu/images/services/multi-use/path-to-student-success-student-version-2x.jpg
 
 mv path-to-student-success-student-version-2x.jpg folder
+
+ll folder
