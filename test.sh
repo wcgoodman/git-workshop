8aaf6338f79ade09d84db07d098f5df9b1783309
Trent Storch <tstorch@bullhorn.com>
2018-10-05 00:21:05 -0500

    adding some simple shell commands

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 ping bullhorn.com
 
 mkdir folder
+
+curl -O https://www.stlcc.edu/images/services/multi-use/path-to-student-success-student-version-2x.jpg
+
+mv path-to-student-success-student-version-2x.jpg folder
